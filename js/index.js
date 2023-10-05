5b5fed081b2d64075ecb36652e137fa86e2a59a7
Gonzalo Maulella <83685692+gonzabb14@users.noreply.github.com>
2023-10-05 12:12:48 -0300

    trabajito casi hecho

--- js/index.js
+++ js/index.js
@@ -26,25 +26,67 @@ searchBtn.addEventListener("click", function(event){
       
 function displayResults(moviesResults, moviesTitle) {
     let pelisArray = [];
+
     moviesResults.forEach(pelicula=>{
-     if (pelicula.title.toLowerCase().includes(moviesTitle.toLowerCase()) || 
-     pelicula.tagline.toLowerCase().includes(moviesTitle.toLowerCase()) || 
-     pelicula.overview.toLowerCase().includes(moviesTitle.toLowerCase())) {
-        pelisArray.push(pelicula);
-     }  
-else {
-    pelicula.genres.forEach(genre => {
-        if ( genre.name.toLowerCase().includes === moviesTitle.toLowerCase()) {
-           
-        }
-     })
-}
-   
-     
+        const even = (element) => element.name.toLowerCase().includes(moviesTitle.toLowerCase());
+        if (    pelicula.title.toLowerCase().includes(moviesTitle.toLowerCase()) || 
+                pelicula.tagline.toLowerCase().includes(moviesTitle.toLowerCase()) || 
+                pelicula.overview.toLowerCase().includes(moviesTitle.toLowerCase()) || 
+                pelicula.genres.some(even)) {
+            pelisArray.push(pelicula);
+        }  
     })
 
-    console.log(pelisArray);
+    let mostrarPeliculas = ``;
 
+    pelisArray.forEach(element => {
+        mostrarPeliculas += `
+        <li class="li" data-score="${element.vote_average}">
+            <div style="display: flex;
+                    justify-content: space-between;
+                    width: 60vw;
+                    border: 0px solid white;">
+                <div style="margin: 5px;">
+                    <p  style="color: white;
+                        margin: 5px; 
+                        font-size: 18px">
+                        <strong>${element.title}</strong>
+                    </p>
+                    <p style="  color: gray; 
+                                margin: 5px;">
+                        ${element.tagline}
+                    </p>
+                </div>
+                <div style="margin-top: 10px;">
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </div>
+            </div>
+      </li>
+        `;
+    })
+    document.getElementsByClassName("list-group container")[0].innerHTML = mostrarPeliculas;
+  
+    Array.from(document.getElementsByClassName("li")).forEach(element => {
+        //console.log(element);
+        console.log(element.getElementsByClassName("fa fa-star"));
+        //Array.from.namedocument.getElementsByTagName("span")
+        let ss =  element.getElementsByClassName("fa fa-star");
+  
+        for (let i = 0; i < parseInt(element.dataset.score/2); i++){
+           console.log(ss[i]);
+           ss[i].classList.add("checked");
+        }
+   
+    })
+
+   
+
+
+    console.log(pelisArray);
 }
 
  
