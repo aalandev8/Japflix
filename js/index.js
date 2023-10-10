bb5e227ac7d8ed63a3aef99500a5d98b382a6ddd
agusgonzaro <agug2001@gmail.com>
2023-10-10 11:07:54 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,39 +2,39 @@ fetch("https://japceibal.github.io/japflix_api/movies-data.json")
     .then(response => {
         console.log(response.ok);
         return response.json();
-        
-    
+
+
     })
     .then(data => {
-       console.log(data);
-       localStorage.setItem("Movies Array",JSON.stringify(data));
-        
-    }) 
+        console.log(data);
+        localStorage.setItem("Movies Array", JSON.stringify(data));
+
+    })
     .catch(error => {
-    console.log(error.message);
-      });
+        console.log(error.message);
+    });
 
 const searchForm = document.getElementById("inputBuscar");
 const moviesContainer = document.getElementById("list-group container");
 const searchBtn = document.getElementById("btnBuscar");
 
-searchBtn.addEventListener("click", function(event){
+searchBtn.addEventListener("click", function (event) {
     const movies = searchForm.value;
     const moviesArray = JSON.parse(localStorage.getItem("Movies Array"));
     displayResults(moviesArray, movies);
- })
-      
+})
+
 function displayResults(moviesResults, moviesTitle) {
     let pelisArray = [];
 
-    moviesResults.forEach(pelicula=>{
+    moviesResults.forEach(pelicula => {
         const even = (element) => element.name.toLowerCase().includes(moviesTitle.toLowerCase());
-        if (    pelicula.title.toLowerCase().includes(moviesTitle.toLowerCase()) || 
-                pelicula.tagline.toLowerCase().includes(moviesTitle.toLowerCase()) || 
-                pelicula.overview.toLowerCase().includes(moviesTitle.toLowerCase()) || 
-                pelicula.genres.some(even)) {
+        if (pelicula.title.toLowerCase().includes(moviesTitle.toLowerCase()) ||
+            pelicula.tagline.toLowerCase().includes(moviesTitle.toLowerCase()) ||
+            pelicula.overview.toLowerCase().includes(moviesTitle.toLowerCase()) ||
+            pelicula.genres.some(even)) {
             pelisArray.push(pelicula);
-        }  
+        }
     })
 
     let mostrarPeliculas = ``;
@@ -42,7 +42,7 @@ function displayResults(moviesResults, moviesTitle) {
     pelisArray.forEach(element => {
         mostrarPeliculas += `
         <li class="li" data-score="${element.vote_average}">
-            <div style="display: flex;
+            <div id="peli" style="display: flex;
                     justify-content: space-between;
                     width: 60vw;
                     border: 0px solid white;">
@@ -69,27 +69,53 @@ function displayResults(moviesResults, moviesTitle) {
         `;
     })
     document.getElementsByClassName("list-group container")[0].innerHTML = mostrarPeliculas;
-  
+
     Array.from(document.getElementsByClassName("li")).forEach(element => {
         //console.log(element);
         console.log(element.getElementsByClassName("fa fa-star"));
         //Array.from.namedocument.getElementsByTagName("span")
-        let ss =  element.getElementsByClassName("fa fa-star");
-  
-        for (let i = 0; i < parseInt(element.dataset.score/2); i++){
-           console.log(ss[i]);
-           ss[i].classList.add("checked");
+        let ss = element.getElementsByClassName("fa fa-star");
+
+        for (let i = 0; i < parseInt(element.dataset.score / 2); i++) {
+            console.log(ss[i]);
+            ss[i].classList.add("checked");
         }
-   
+
     })
 
-   
+
 
 
     console.log(pelisArray);
 }
+document.getElementsByClassName("li").addEventListener("click", () => {
+    const htmlContent = `< button class="btn btn-primary" type = "button" data - bs - toggle="offcanvas" data - bs - target="#offcanvasTop" aria - controls="offcanvasTop" > Toggle top offcanvas</button >
 
- 
+    <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasTopLabel">${pelicula.title}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body"> ${pelicula.overview}
+            <li><hr class="dropdown-divider"></li>
+            ${pelicula.genres.name}
+        </div>
+    </div>
+      <div class="dropdown mt-3">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+          More
+        </button>
+        <ul class="dropdown-menu">
+          <li><p class="dropdown-item">Year: ${pelicula.release_date.getFullYear()}</p></li>
+          <li><p class="dropdown-item">Runtime: ${pelicula.runtime}</p></li>
+          <li><p class="dropdown-item">Budget: $ ${pelicula.budget}</p></li>
+          <li><p class="dropdown-item">Revenue: $ ${pelicula.revenue}</p></li>
+        </ul>
+      </div>
+    </div>
+  </div>`
+    document.getElementsByClassName("li").innerHTML = htmlContent;
+})
 
 
 
